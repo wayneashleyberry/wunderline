bb3ac4d699bbfc00570296ce70ba9cc420a9c9ca
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-05-06 10:45:19 +0200

    fixes

--- commands/whoami.js
+++ commands/whoami.js
@@ -1,18 +1,17 @@
-var sdk = require('wunderlist');
-var conf = {};
-require('rc')('wunderlist-cli', conf);
+var SDK = require('wunderlist')
+var conf = {}
+require('rc')('wunderlist-cli', conf)
 
-var api = new sdk({
+var api = new SDK({
   'accessToken': conf.access_token,
   'clientID': conf.client_id
-});
+})
 
+module.exports = function () {
+  var req = api.http.user.all()
 
-module.exports = function() {
-  var req = api.http.user.all();
-
-  req.then(function(user) {
-    console.log(user.name + ' <' + user.email + '>');
-    process.exit();
-  });
+  req.then(function (user) {
+    console.log(user.name + ' <' + user.email + '>')
+    process.exit()
+  })
 }
--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var cli = require('cli')
 var fs = require('fs')
 
-var conf = {};
-require('rc')('wunderlist-cli', conf);
+var conf = {}
+require('rc')('wunderlist-cli', conf)
 
 var packageJson = fs.readFileSync(__dirname + '/package.json', {encoding: 'utf-8'})
 packageJson = JSON.parse(packageJson)
@@ -15,28 +15,28 @@ cli.enable('version')
 var commands = {
   add: [null, 'Add a new task to your inbox', 'task'],
   whoami: [null, 'Display effective user'],
-  flush: [null, 'Flush the application cache'],
+  flush: [null, 'Flush the application cache']
 }
 
 cli.parse(commands, ['add', 'whoami', 'flush'])
 
 if (cli.command === 'add') {
-  var command = require('./commands/add');
+  var command = require('./commands/add')
   command({
     title: cli.args.join(' ')
-  });
+  })
 }
 
 if (cli.command === 'whoami') {
-  var command = require('./commands/whoami');
-  command();
+  var command = require('./commands/whoami')
+  command()
 }
 
 if (cli.command === 'flush') {
   try {
-    fs.unlinkSync(__dirname + '/cache.db');
+    fs.unlinkSync(__dirname + '/cache.db')
   } catch (e) {
     //
   }
-  process.exit();
+  process.exit()
 }
