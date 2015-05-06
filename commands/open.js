b4920bd937dba58a8a2c87ce402e5eafe33db551
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-05-06 14:33:36 +0200

    standard style fixes

--- commands/open.js
+++ commands/open.js
@@ -1,11 +1,11 @@
-var opn = require('opn');
+var opn = require('opn')
 var conf = {platform: 'web'}
 require('rc')('wunderlist-cli', conf)
 
 if (conf.platform === 'mac') {
-  opn('wunderlist://');
-  process.exit();
+  opn('wunderlist://')
+  process.exit()
 } else {
-  opn('https://www.wunderlist.com/#/lists/inbox');
-  process.exit();
+  opn('https://www.wunderlist.com/#/lists/inbox')
+  process.exit()
 }
