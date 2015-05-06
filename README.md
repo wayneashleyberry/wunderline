93beb0783c449c1ae4f151c88a062cc08e2daf1d
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-05-06 11:24:23 +0200

    cleanup

--- README.md
+++ README.md
@@ -46,9 +46,6 @@ Usage:
   wunderlist-cli [OPTIONS] <command> [ARGS]
 
 Options:
-      --add TASK         Add a new task to your inbox
-      --whoami           Display effective user
-      --flush            Flush the application cache
   -v, --version          Display the current version
   -h, --help             Display help and usage details
 
--- index.js
+++ index.js
@@ -12,13 +12,7 @@ packageJson = JSON.parse(packageJson)
 cli.setApp('wunderlist-cli', packageJson.version)
 cli.enable('version')
 
-var commands = {
-  add: [null, 'Add a new task to your inbox', 'task'],
-  whoami: [null, 'Display effective user'],
-  flush: [null, 'Flush the application cache']
-}
-
-cli.parse(commands, ['add', 'whoami', 'flush'])
+cli.parse(null, ['add', 'whoami', 'flush'])
 
 if (cli.command === 'add') {
   var command = require('./commands/add')
