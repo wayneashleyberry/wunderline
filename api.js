5e7ad5c9ff6c7b7a1dc06dca1d8d893bcd5d9b1e
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-05-06 15:47:08 +0200

    debug mode

--- api.js
+++ api.js
@@ -1,5 +1,13 @@
+var cli = require('cli')
 var SDK = require('wunderlist')
 var conf = require('./config')
+
+if (cli.options.debug) {
+  SDK.prototype.setupLogging({
+    'logLevel': 'debug',
+    'logPattern': '*'
+  });
+}
 
 var api = new SDK({
   'accessToken': conf.access_token,
