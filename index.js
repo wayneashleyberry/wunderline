586b20ef81c1901023e84800741f07da5f6eea79
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-05-06 10:10:00 +0200

    adds `add` and `whoami` commands

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 var cli = require('cli')
 var fs = require('fs')
+var sdk = require('wunderlist');
+
+var conf = {};
+require('rc')('wunderlist-cli', conf);
 
 var packageJson = fs.readFileSync(__dirname + '/package.json', {encoding: 'utf-8'})
 packageJson = JSON.parse(packageJson)
@@ -9,12 +13,39 @@ packageJson = JSON.parse(packageJson)
 cli.setApp('wunderlist-cli', packageJson.version)
 cli.enable('version')
 
+var api = new sdk({
+  'accessToken': conf.access_token,
+  'clientID': conf.client_id
+});
+
 var commands = {
-  add: ['a', 'Add a new task to your inbox', 'task']
+  add: ['a', 'Add a new task to your inbox', 'task'],
+  whoami: [null, 'Display effective user'],
 }
 
-cli.parse(commands, ['add'])
+cli.parse(commands, ['add', 'whoami'])
 
 if (cli.command === 'add') {
-  //
+  var title = cli.args.join(' ');
+  var req = api.http.lists.all();
+  req.then(function(res) {
+    var list_id = res[0].id;
+    var req = api.http.tasks.create({
+      list_id: list_id,
+      title: title
+    });
+    req.then(function(res) {
+      console.log('Created task ' + res.id);
+      process.exit();
+    });
+  });
+}
+
+if (cli.command === 'whoami') {
+  var req = api.http.user.all();
+
+  req.then(function(user) {
+    console.log(user.name + ' <' + user.email + '>');
+    process.exit();
+  });
 }
