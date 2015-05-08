ba4679154513c5210f278eb95b7a035e009204da
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-05-08 13:16:44 +0200

    cleanup

--- wunderlist.js
+++ wunderlist.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 
 var app = require('commander')
-var fs = require('fs')
-
-var json = JSON.parse(fs.readFileSync(__dirname + '/package.json'))
+var pckg = require('./package.json')
 
 app
-  .version(json.version)
+  .version(pckg.version)
   .command('ls', 'List all of your tasks')
   .command('add [task]', 'Add a task to your inbox')
   .command('open', 'Open Wunderlist')
