22815d723a84afb4a564806b87b6a85f4c462eb1
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-05-11 14:31:32 +0200

    adds filtering for week command

--- wunderlist-week.js
+++ wunderlist-week.js
@@ -1,9 +1,29 @@
 #!/usr/bin/env node
 
 var app = require('commander')
+var moment = require('moment')
+var lists = require('./lists')
+var print = require('./print')
 
 app
   .description('View tasks due this week')
   .parse(process.argv)
 
-// TODO
+lists(function (err, data) {
+  if (err) process.exit(-1)
+
+  var week = moment().format('w')
+
+  data.sort(function (a, b) {
+    if (a.title === 'inbox') return -1
+    if (b.title === 'inbox') return 1
+    return a.title > b.title
+  }).forEach(function (list) {
+    list.tasks = list.tasks.filter(function(item) {
+      if (! item.due_date) return false
+      return moment(item.due_date).format('w') === week
+    })
+
+    print(list)
+  })
+})
