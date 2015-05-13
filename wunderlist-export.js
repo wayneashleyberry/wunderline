79526146edc34b6ef44439cac496a03f66ca0ce3
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-05-13 14:57:24 +0200

    export notes, subtasks and files

--- wunderlist-export.js
+++ wunderlist-export.js
@@ -27,18 +27,71 @@ async.waterfall([
     })
   },
   function (data, callback) {
-    var tasks = []
+    var lists = []
     async.each(data.user.lists, function (list, complete) {
-      api.get({url: '/tasks', qs: {list_id: list.id}}, function (err, res, body) {
-        progress()
-        tasks[list.id] = body
+      async.parallel([
+        function getTasks (cb) {
+          api.get({url: '/tasks', qs: {list_id: list.id}}, function (err, res, body) {
+            progress()
+            cb(err, body)
+          })
+        },
+        function getSubtasks (cb) {
+          api.get({url: '/subtasks', qs: {list_id: list.id}}, function (err, res, body) {
+            progress()
+            cb(err, body)
+          })
+        },
+        function getNotes (cb) {
+          api.get({url: '/notes', qs: {list_id: list.id}}, function (err, res, body) {
+            progress()
+            cb(err, body)
+          })
+        },
+        function getFiles (cb) {
+          api.get({url: '/files', qs: {list_id: list.id}}, function (err, res, body) {
+            progress()
+            cb(err, body)
+          })
+        }
+      ], function (err, results) {
+        var tasks = results[0]
+        var subtasks = results[1]
+        var notes = results[2]
+        var files = results[3]
+        tasks.forEach(function (task, index) {
+          tasks[index].subtasks = []
+          tasks[index].notes = []
+          tasks[index].files = []
+        })
+        subtasks.forEach(function (subtask) {
+          tasks.forEach(function (task, index) {
+            if (task.id === subtask.task_id) {
+              tasks[index].subtasks.push(subtask)
+            }
+          })
+        })
+        notes.forEach(function (note) {
+          tasks.forEach(function (task, index) {
+            if (task.id === note.task_id) {
+              tasks[index].notes.push(note)
+            }
+          })
+        })
+        files.forEach(function (file) {
+          tasks.forEach(function (task, index) {
+            if (task.id === file.task_id) {
+              tasks[index].files.push(note)
+            }
+          })
+        })
+        list.tasks = tasks
+        lists.push(list)
         complete()
       })
-    }, function () {
-      data.user.lists.forEach(function (list, index) {
-        data.user.lists[index].tasks = tasks[list.id]
-      })
-      callback(null, data)
+    }, function (err) {
+      data.user.lists = lists
+      callback(err, data)
     })
   }
 ], function (err, data) {
