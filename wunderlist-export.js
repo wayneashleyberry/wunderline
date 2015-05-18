a6b6982dce19f2e51bc66bd04d2d170b260280dd
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-05-18 16:18:00 +0200

    move function definitions

--- wunderlist-export.js
+++ wunderlist-export.js
@@ -12,98 +12,104 @@ function showProgress () {
   process.stderr.write('.')
 }
 
-async.waterfall([
-  function getUser(callback) {
-    api('/user', function (err, res, body) {
+function getUser(callback) {
+  api('/user', function (err, res, body) {
+    if (err) process.exit(1)
+
+    showProgress()
+    callback(null, {user: body})
+  })
+}
+
+function getLists(data, callback) {
+  api('/lists', function (err, res, body) {
+    if (err) process.exit(1)
+
+    showProgress()
+    data.user.lists = body
+    callback(null, data)
+  })
+}
+
+function embedLists(data, callback) {
+  var lists = []
+  async.each(data.user.lists, function (list, complete) {
+    async.parallel([
+      function getTasks (cb) {
+        api({url: '/tasks', qs: {list_id: list.id}}, function (err, res, body) {
+          showProgress()
+          cb(err, body)
+        })
+      },
+      function getSubtasks (cb) {
+        api({url: '/subtasks', qs: {list_id: list.id}}, function (err, res, body) {
+          showProgress()
+          cb(err, body)
+        })
+      },
+      function getNotes (cb) {
+        api({url: '/notes', qs: {list_id: list.id}}, function (err, res, body) {
+          if (err) process.exit(1)
+
+          showProgress()
+          cb(err, body)
+        })
+      },
+      function getFiles (cb) {
+        api({url: '/files', qs: {list_id: list.id}}, function (err, res, body) {
+          showProgress()
+          cb(err, body)
+        })
+      }
+    ], function (err, results) {
       if (err) process.exit(1)
 
-      showProgress()
-      callback(null, {user: body})
-    })
-  },
-  function getLists(data, callback) {
-    api('/lists', function (err, res, body) {
-      if (err) process.exit(1)
-
-      showProgress()
-      data.user.lists = body
-      callback(null, data)
-    })
-  },
-  function embedLists(data, callback) {
-    var lists = []
-    async.each(data.user.lists, function (list, complete) {
-      async.parallel([
-        function getTasks (cb) {
-          api({url: '/tasks', qs: {list_id: list.id}}, function (err, res, body) {
-            showProgress()
-            cb(err, body)
-          })
-        },
-        function getSubtasks (cb) {
-          api({url: '/subtasks', qs: {list_id: list.id}}, function (err, res, body) {
-            showProgress()
-            cb(err, body)
-          })
-        },
-        function getNotes (cb) {
-          api({url: '/notes', qs: {list_id: list.id}}, function (err, res, body) {
-            if (err) process.exit(1)
-
-            showProgress()
-            cb(err, body)
-          })
-        },
-        function getFiles (cb) {
-          api({url: '/files', qs: {list_id: list.id}}, function (err, res, body) {
-            showProgress()
-            cb(err, body)
-          })
-        }
-      ], function (err, results) {
-        if (err) process.exit(1)
-
-        var tasks = results[0]
-        var subtasks = results[1]
-        var notes = results[2]
-        var files = results[3]
-        tasks.forEach(function (task, index) {
-          tasks[index].subtasks = []
-          tasks[index].notes = []
-          tasks[index].files = []
-        })
-        subtasks.forEach(function (subtask) {
-          tasks.forEach(function (task, index) {
-            if (task.id === subtask.task_id) {
-              tasks[index].subtasks.push(subtask)
-            }
-          })
-        })
-        notes.forEach(function (note) {
-          tasks.forEach(function (task, index) {
-            if (task.id === note.task_id) {
-              tasks[index].notes.push(note)
-            }
-          })
-        })
-        files.forEach(function (file) {
-          tasks.forEach(function (task, index) {
-            if (task.id === file.task_id) {
-              tasks[index].files.push(file)
-            }
-          })
-        })
-        list.tasks = tasks
-        lists.push(list)
-        complete()
+      var tasks = results[0]
+      var subtasks = results[1]
+      var notes = results[2]
+      var files = results[3]
+      tasks.forEach(function (task, index) {
+        tasks[index].subtasks = []
+        tasks[index].notes = []
+        tasks[index].files = []
       })
-    }, function (err) {
-      if (err) process.exit(1)
-
-      data.user.lists = lists
-      callback(err, data)
+      subtasks.forEach(function (subtask) {
+        tasks.forEach(function (task, index) {
+          if (task.id === subtask.task_id) {
+            tasks[index].subtasks.push(subtask)
+          }
+        })
+      })
+      notes.forEach(function (note) {
+        tasks.forEach(function (task, index) {
+          if (task.id === note.task_id) {
+            tasks[index].notes.push(note)
+          }
+        })
+      })
+      files.forEach(function (file) {
+        tasks.forEach(function (task, index) {
+          if (task.id === file.task_id) {
+            tasks[index].files.push(file)
+          }
+        })
+      })
+      list.tasks = tasks
+      lists.push(list)
+      complete()
     })
-  }
+  }, function (err) {
+    if (err) process.exit(1)
+
+    data.user.lists = lists
+    callback(err, data)
+  })
+}
+
+async.waterfall([
+  getUser,
+  getLists,
+  embedLists
 ], function (err, data) {
   if (err) process.exit(1)
 
