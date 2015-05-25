b1753c4a9ca578a981832ae484df9b276cfa34a2
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-05-25 17:39:58 +0200

    adds basic test

---
+++ test.js
@@ -0,0 +1,13 @@
+var test = require('tape')
+var binCheck = require('bin-check')
+
+test('check bin', function (t) {
+  t.plan(1)
+
+  binCheck('./wunderline.js', function (err, works) {
+    if (err) {
+      //
+    }
+    t.ok(works, './wunderline.js has a zero exit code')
+  })
+})
