514b1e2995d0026a585859d26e6cdb2e259bee48
Michael Thorne <michael.thorne@userx.co.za>
2015-06-25 15:11:53 +0200

    Added `grunt-html` [#37]

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,13 @@ module.exports = function (grunt) {
     grunt.initConfig({
         eslint: {
             target: ['assets/js/main.js']
+        },
+        htmllint: {
+            all: ['_site/*.html', '!_site/googled2961d1b909d4f84.html']
         }
     });
 
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('test', ['eslint']);
+    grunt.registerTask('test', ['eslint', 'htmllint']);
 };
