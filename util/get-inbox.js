735b0cac15ee085d7d08e97a41ceb62198952863
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-07-03 10:51:14 +0200

    adds inbox response debugging

--- util/get-inbox.js
+++ util/get-inbox.js
@@ -1,6 +1,7 @@
 var api = require('./api')
 var fs = require('fs')
 var path = __dirname + '/../cache.json'
+var request = require('request')
 
 try {
   var cache = require(path)
@@ -9,12 +10,16 @@ try {
 }
 
 module.exports = function getInbox (cb) {
-  if (cache.inbox) {
-    return cb(cache.inbox)
-  }
+  // if (cache.inbox) {
+  //   return cb(cache.inbox)
+  // }
 
   api('/lists', function (err, res, body) {
-    if (err || body.error || ! body || body.length < 1) {
+    if (request.debug) {
+      console.log('REQUEST', 'response', 'body', body)
+    }
+
+    if (err || body.error) {
       console.log(err || body.error)
       process.exit(1)
     }
