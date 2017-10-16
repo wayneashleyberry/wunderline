d424e2b8201a48642ae856b705596835071cfa06
Thor77 <thor77@thor77.org>
2017-10-16 09:06:30 +0200

    Add missing callback-call after subtask-requests

--- wunderline-add.js
+++ wunderline-add.js
@@ -178,6 +178,12 @@ function main() {
                   }
                 }
               );
+            }, function(error) {
+              if (error) {
+                callback(error, null)
+              } else {
+                callback(null, task)
+              }
             });
           } else {
             callback(null, task);
